97c0fd30669a157b4256e671bc841c624d71057c
hikarido pechka <tocin723@gmail.com>
2019-08-11 23:27:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# stm32_7_segment
---
+++ Src/main.c
@@ -0,0 +1,274 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+void set_number_on_led(int number);
+void clear_leds();
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  int i = -1;
+  while (1)
+  {
+	if(i > 8){
+		i = -1;
+	}
+
+	set_number_on_led(i);
+	HAL_Delay(1000);
+	i++;
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, A_Pin|F_Pin|E_Pin|D_Pin 
+                          |C_Pin|B_Pin|G_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : A_Pin F_Pin E_Pin D_Pin 
+                           C_Pin B_Pin G_Pin */
+  GPIO_InitStruct.Pin = A_Pin|F_Pin|E_Pin|D_Pin 
+                          |C_Pin|B_Pin|G_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+/*
+ * set leds as number according to number
+ * is number not in [0, 1,2,3,4,5,6,7, 8]
+ * leds 3,4,5,6,9 will be set(like E sing)
+ */
+void set_number_on_led(int number){
+	// E sing for errors
+	if(number < 0 || number > 8){
+		HAL_GPIO_WritePin(
+			GPIOB,
+			A_Pin | F_Pin | E_Pin | D_Pin | G_Pin,
+			GPIO_PIN_SET
+		);
+		return;
+	}
+
+	uint16_t outs_for_number = 0;
+
+	switch(number){
+		case 0:
+			outs_for_number = A_Pin | F_Pin | E_Pin |
+							  D_Pin | C_Pin | B_Pin;
+		break;
+
+		case 1:
+			outs_for_number = B_Pin | C_Pin;
+		break;
+
+		case 2:
+			outs_for_number = A_Pin | B_Pin | G_Pin | D_Pin | E_Pin;
+		break;
+
+		case 3:
+			outs_for_number = A_Pin | B_Pin | C_Pin | D_Pin | G_Pin;
+		break;
+
+		case 4:
+			outs_for_number = F_Pin | G_Pin | B_Pin | C_Pin;
+		break;
+
+		case 5:
+			outs_for_number = A_Pin | F_Pin | G_Pin | C_Pin | D_Pin;
+		break;
+
+		case 6:
+			outs_for_number = A_Pin | F_Pin | G_Pin | C_Pin | D_Pin | E_Pin;
+		break;
+
+		case 7:
+			outs_for_number = A_Pin | B_Pin | C_Pin;;
+		break;
+
+		case 8:
+			outs_for_number = A_Pin | B_Pin | C_Pin | D_Pin | E_Pin |
+							  F_Pin | G_Pin;
+		break;
+	}
+
+	clear_leds();
+	HAL_GPIO_WritePin(GPIOB, outs_for_number, GPIO_PIN_SET);
+}
+
+void clear_leds(){
+	uint16_t leds = A_Pin | B_Pin | C_Pin |
+			        D_Pin | E_Pin | F_Pin | G_Pin;
+	HAL_GPIO_WritePin(GPIOB, leds, GPIO_PIN_RESET);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
